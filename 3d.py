062f84e4b063cf029caf68cd0860ec5f806028a9
ArnaudBand <bandonkeyea@gmail.com>
2023-10-12 09:47:52 +0300

    Generate random 3D data

---
+++ 3d.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+
+# Generate random 3d data
+x = np.random.random(100)
+y = np.random.random(100)
+z = np.random.random(100)
